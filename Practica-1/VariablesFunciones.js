32e3af84f9c39b84324944a591ea71c6e1f381ab
Lucero Cuellar <122043929@upq.edu.mx>
2025-05-16 17:59:19 -0600

    código c
    cree una función arrow que contiene los parámetros de nombre y edad y este te devolverá un saludo personalizado

--- Practica-1/VariablesFunciones.js
+++ Practica-1/VariablesFunciones.js
@@ -8,3 +8,8 @@ console.log(saludo);
 // función arrow
 const cuadrado = (numero) => numero * numero;
 console.log(cuadrado(5));
+
+const saludoPersonalizado = (nombre, edad) => {
+    return `Hola soy ${nombre} y tengo ${edad} años`;
+}
+console.log(saludoPersonalizado('Armando', 25));
