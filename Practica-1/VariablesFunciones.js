bbaf9d755b2865be3f93521881724e02b2092cc7
Lucero Cuellar <122043929@upq.edu.mx>
2025-05-16 15:29:07 -0600

    Código b
    Cambié la función normal a una función arrow, más simple

--- Practica-1/VariablesFunciones.js
+++ Practica-1/VariablesFunciones.js
@@ -4,3 +4,7 @@ nombre = 'Ana María';
 
 let saludo = 'Hola soy ' + nombre + 'y tengo ' + edad + ' años';
 console.log(saludo);
+
+// función arrow
+const cuadrado = (numero) => numero * numero;
+console.log(cuadrado(5));
