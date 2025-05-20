003643f0aa44d7274f4bc67448ed04afa0d4baf6
Lucero Cuellar <122043929@upq.edu.mx>
2025-05-19 18:45:16 -0600

    Ejercicio B
    En este ejercicio, utiliza el método filter, que permite filtrar, devolver, los elementos que coincidan con la condición declarada, sin modificar el original

---
+++ Practica-2/ejercicioB.js
@@ -0,0 +1,10 @@
+//Ejercicio b
+const productos =[
+    {nombre: "Laptop", precio: 1200},
+    {nombre: "Mouse", precio: 250},
+    {nombre: "Teclado", precio: 750},
+    {nombre: "Monitor", precio: 3000},
+];
+
+const productosFiltrados = productos.filter((producto) => producto.precio >1000);
+console.log(productosFiltrados);
