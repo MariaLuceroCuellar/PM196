1626f5c98154e5821470e567f6dea14036bc4e3b
Lucero Cuellar <122043929@upq.edu.mx>
2025-06-13 14:44:53 -0600

    Propiedad children

--- my-blank-app/App.js
+++ my-blank-app/App.js
@@ -2,19 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
     const Texto = (props)=>{
-      const {contenido} = props;
+      const {children} = props;
       return (
-        <Text> {contenido} </Text>
+        <Text> {children} </Text>
       )
       
     }
 export default function App() {
   return (
     <View style={styles.container}>
-      <Texto contenido = "Hola"/>
-      <Texto contenido = "¿Comó estas?"/>
-      <Texto contenido = "Mi nombre es Lucero"/>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Texto> "Hola" </Texto>
+      <Texto> "Mundo" </Texto>
       <Button title="Click Me" />
       <StatusBar style="auto" />
     </View>
